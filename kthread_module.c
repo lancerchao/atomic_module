0cc11c363b0ffbc0c37b242d388c113e07d45969
Lance Chao <lancerchao@gmail.com>
2017-04-25 16:05:51 -0700

    Fixed kthread_stop

--- kthread_module.c
+++ kthread_module.c
@@ -14,45 +14,57 @@ MODULE_DESCRIPTION("kthreads");
 #define NUM_ITER 1000000
 struct semaphore beginSema1, beginSema2, endSema;
 
+#ifdef BUILTIN
 int X, Y;
+#else
+atomic_t X, Y;
+#endif
 int r1, r2;
+bool done;
 
 static struct task_struct *tids[N];
-static bool done[N] = {false};
 
 static int thread1_routine(void *data) 
 {
-    while (!kthread_should_stop())
+    while (!done)
     {
         down(&beginSema1);  // Wait for signal from main thread
         while (get_random_int() % 8 != 0) {
         }  // Add a short, random delay
  
         // ----- THE TRANSACTION! -----
-        //__atomic_store_n(&X, 1, __ATOMIC_SEQ_CST);
-        X = 1;
-        //asm volatile("" ::: "memory");  // Prevent compiler reordering
-        r1 = Y;
- 
+
+#ifdef BUILTIN
+        __atomic_store_n(&X, 1, __ATOMIC_SEQ_CST);
+#else
+        atomic_set(&X, 1);
+        asm volatile("" ::: "memory");  // Prevent compiler reordering
+        r1 = atomic_read(&Y);
+#endif 
         up(&endSema);  // Notify transaction complete
     }
+
     return 0; 
 }
 
 static int thread2_routine(void *data) 
 {
-    while (!kthread_should_stop())
+    while (!done)
     {
         down(&beginSema2);  // Wait for signal from main thread
         while (get_random_int() % 8 != 0) {
         }  // Add a short, random delay
  
         // ----- THE TRANSACTION! -----
-        //__atomic_store_n(&Y, 1, __ATOMIC_SEQ_CST);
-        Y = 1;
-        //asm volatile("" ::: "memory");  // Prevent compiler reordering
-        r2 = X;
- 
+
+#ifdef BUILTIN
+        __atomic_store_n(&Y, 1, __ATOMIC_SEQ_CST);
+#else
+        atomic_set(&Y, 1);
+        asm volatile("" ::: "memory");  // Prevent compiler reordering
+        r2 = atomic_read(&X);
+#endif 
+
         up(&endSema);  // Notify transaction complete
     }
     return 0;  // Never returns
@@ -85,6 +97,7 @@ int init_module(void)
     sema_init(&beginSema2, 0);
     sema_init(&endSema, 0);
  
+    done = false;
 
     // Spawn the threads
     tids[0] = kthread_run(thread1_routine, NULL, "spawn");
@@ -96,8 +109,15 @@ int init_module(void)
     for (iterations = 1; iterations < NUM_ITER; iterations++)
     {
         // Reset X and Y
+
+#ifdef BUILTIN
         X = 0;
         Y = 0;
+#else
+        atomic_set(&X, 0);
+        atomic_set(&Y, 0);
+#endif
+
         // Signal both threads
         up(&beginSema1);
         up(&beginSema2);
@@ -114,10 +134,6 @@ int init_module(void)
         if (iterations % 10000 == 0)
             printk(KERN_INFO "%d iterations\n", iterations);
     }
-    kthread_stop(tids[0]);
-    kthread_stop(tids[1]);
-    up(&beginSema1);
-    up(&beginSema2);
     return 0;  // Never returns
 
 }
@@ -125,4 +141,9 @@ int init_module(void)
 void cleanup_module(void)
 { 
     printk(KERN_INFO "Stopping kthread_module\n"); 
+    done = true;
+    up(&beginSema1);
+    up(&beginSema2);
+    kthread_stop(tids[0]);
+    kthread_stop(tids[1]);
 }
